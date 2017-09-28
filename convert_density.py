340a52bc516d40d0ab11dc8cd6754eea3c6f4c63
Chaitanya Krishna Ande <icymist@gmail.com>
2017-09-29 00:19:05 +0200

    added lines to print thin film units

--- convert_density.py
+++ convert_density.py
@@ -1,11 +1,12 @@
 import ase
 from ase.data import atomic_numbers, atomic_masses
 
-density = 1.77  # g/cm^3
+density = 1.73  # g/cm^3
 
 Na = 6.022e23
 
 atom = 'B'
+thickness = 4.5 #nm
 
 molecular_mass = atomic_masses[atomic_numbers[atom]]
 
@@ -15,4 +16,13 @@ natoms = nmoles * Na
 
 atoms_per_ang_3 = natoms / (1e8)**3
 
+atoms_per_cm_3 = natoms
+
+thickness_cm = thickness * 1e-9 * 1e2
+
+tfu = atoms_per_cm_3 * thickness_cm
+tfu = tfu / 1e15
+
 print(atoms_per_ang_3)
+print(atoms_per_cm_3)
+print(tfu)
